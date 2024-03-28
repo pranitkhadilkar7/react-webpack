3aea1b5c75a39ee886ece5bb3e0ae006fa946e57
Pranit Khadilkar <pranit@emtech.com>
2024-03-28 18:29:51 +0530

    added dev server configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/index.js',
     devtool: 'source-map',
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        open: true
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/[name].[contenthash].js',
